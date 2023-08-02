2ea5ce051a52f95c04193d301bca7f56a86f3dc8
stupnikk <andre.lacassagne82@gmail.com>
2023-08-02 10:28:17 +0200

    ok

--- src/svg/svg.svelte
+++ src/svg/svg.svelte
@@ -2,10 +2,11 @@
 import { SVG }  from "@svgdotjs/svg.js"
 import { onMount } from "svelte";
 
-$: x = 120
+$: x = 100
+$: width = 2 
 
-function draw3DVectors(x:number) {
-  var draw = SVG().addTo("#app").size(300, 300);
+function draw3Axis(x:number, width:number) {
+  var draw = SVG().addTo("#app").size(200, 200);
 
   // Coordinates of the origin point
   
@@ -14,34 +15,32 @@ function draw3DVectors(x:number) {
   var vectorLength = x/3 ;
 
   // Length of the vectors
-  
+  function drawLine( arr1:[number, number], arr2:[number, number], color:string){
+    var xAxis = draw.line( arr1[0] , arr1[1], arr2[0], arr2[1]).stroke({ color: color, width: width });
+  }
 
   // Draw the x-axis vector (red)
-  var xAxis = draw.line(originX, originY, originX + vectorLength, originY)
-                 .stroke({ color: '#f44336', width: 3 });
+  drawLine([originX, originY], [originX + vectorLength, originY], "red")
+  drawLine([originX, originY], [originX, originY - vectorLength], "blue")
 
   // Draw the x-axis arrowhead (red triangle)
-  var xArrowhead = draw.polygon(`${originX + vectorLength},${originY - 5} ${originX + vectorLength},${originY + 5} ${originX + vectorLength + 15},${originY}`)
-                       .fill('#f44336');
+  var xArrowhead = draw.polygon(`${originX + vectorLength},${originY - 5} ${originX + vectorLength},${originY + 5} ${originX + vectorLength + 15},${originY}`).fill('#f44336');
 
   // Draw the y-axis vector (green)
-  var yAxis = draw.line(originX, originY, originX, originY - vectorLength)
-                 .stroke({ color: '#4caf50', width: 3 });
+  
   
   
   // Draw the y-axis arrowhead (green triangle)
-  var yArrowhead = draw.polygon(`${originX - 5},${originY - vectorLength} ${originX + 5},${originY - vectorLength} ${originX},${originY - vectorLength - 15}`)
-                       .fill('#4caf50');
-    yArrowhead.animate().attr({fill:"#4caf50"}).move(150, 0)
+  var yArrowhead = draw.polygon(`${originX - 5},${originY - vectorLength} ${originX + 5},${originY - vectorLength} ${originX},${originY - vectorLength - 15}`).fill('#4caf50');
+    
   // Draw the z-axis vector (blue)
-  var zAxis = draw.line(originX,originY, originX - vectorLength, originY + vectorLength)
-                 .stroke({ color: '#2196f3', width: 3 });
+  var zAxis = draw.line(originX,originY, originX - vectorLength, originY + vectorLength).stroke({ color: '#2196f3', width: width});
 
   // Draw the z-axis arrowhead (blue triangle)
-  var zArrowhead = draw.polygon(`${originX - vectorLength},${originY + vectorLength} ${originX - vectorLength + 5},${originY + vectorLength} ${originX - vectorLength },${originY + vectorLength + 15}`)
-                       .fill('#2196f3');
+  var zArrowhead = draw.polygon(`${originX - vectorLength},${originY + vectorLength - 10 } ${originX  - vectorLength},${originY + vectorLength + 5} ${originX - vectorLength},${originY + vectorLength + 15}`).fill('#4caf50');
 
-  /*              
+  /*    
+  yArrowhead.animate().attr({fill:"#4caf50"}).move(150, 0)          
   function update(){yAxis.plot(originX, originY, originX +  vectorLength , originY - vectorLength)}
   yAxis.click(() => update())
   draw.text("X").move(originX + vectorLength + 20, originY - 10).font({ size: 16, fill: '#f44336' });
@@ -52,7 +51,7 @@ function draw3DVectors(x:number) {
 
 
   onMount(
-    () => {draw3DVectors(x)}
+    () => {draw3Axis(x, width)}
   )
    
 
