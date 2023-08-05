41d7893bc3ccd89f360f4daeafa4129adc8f5c36
stupnikk <andre.lacassagne82@gmail.com>
2023-08-05 22:16:00 +0200

    ok

---
+++ src/components/dropDown.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    export let title:string
+    export let items: string[]
+    $: dropDown = "nodropdown"
+    $: itemHover = "nohover"
+
+</script>
+
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div  class=""
+on:mouseenter={ () => { dropDown = "dropdown"}}  
+on:mouseleave={() => { dropDown = "nodropdown"}}>
+  <a href="/math/matrix" class="relative h-full">{title}</a>
+  <div class={dropDown} >
+      {#each items as item, ind}
+      <li class={itemHover} 
+      id={ind.toString()}
+      on:mouseenter={(e) => { 
+        if(e.target && e.target.id === ind.toString()) itemHover = "hover" 
+        }}  
+      on:mouseleave={() => { dropDown = "nodropdown"}}
+        >{item}</li>
+      {/each}
+  </div>
+</div>
+
+
+<style>
+
+
+.dropdown{
+      position: absolute;
+      padding: 2%;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      background-color: antiquewhite;
+      list-style-type: none;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+    }
+
+    .nodropdown{
+        display: none;
+    }
+
+    .nohover{
+        display: flex;
+    }
+
+    .hover{
+        background-color: aliceblue;
+    }
+</style>
--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,26 +1,32 @@
 <script>
     
     import "@fontsource/voltaire";
+    import DropDown from "./dropDown.svelte";
   
-    let list = ["example", "michel"];
-    $: dropdown = "nodropdown";
+    let mathItems = ["matrix", "vector"];
+    let codeItems = ["python", "javascript"]
+    
+
   
 
   </script>
   
   <header class="bg-indigo-100 voltaire h-8">
         <nav class="h-full flex items-center justify-around">
-            <div>
-                <a href="/math/matrix" class="relative" on:mouseenter={ () => { dropdown = "dropdown"}} 
-                    on:mouseleave={() => { dropdown = "nodropdown"}}>Algebre Linéaire</a>
-                <div class={dropdown}>
-                    {#each list as item}
-                    <li class="item">{item}</li>
-                    {/each}
-                </div>
+            
               
-            </div>
-            <div><a href="/programmation">Programmation</a></div>
+            <DropDown
+            title="Algébre Lineaire"
+            items={mathItems}
+            >
+            </DropDown> 
+            <DropDown
+            title="Programmation"
+            items={codeItems}
+            >
+
+            </DropDown>  
+            
         </nav>
         
   </header>
@@ -30,20 +36,6 @@
       font-family: "Voltaire", sans-serif;
     }
   
-    .dropdown{
-      position: absolute;
-      padding: 2%;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      background-color: antiquewhite;
-      list-style-type: none;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-around;
-    }
 
-    .nodropdown{
-        display: none;
-    }
   </style>
   
