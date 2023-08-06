ca9a6d21dcd302cb7681fae16eb8cfae6efd51a0
stupnikk <andre.lacassagne82@gmail.com>
2023-08-06 16:26:28 +0200

    ok

--- src/components/AsideComp.svelte
+++ src/components/AsideComp.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
 import "@fontsource/voltaire"
-    import { deleteBlank } from "../functions/deleteBlank";
+import { deleteBlank } from "../functions/deleteBlank";
 
+$: hoverStatus = -1
 export let items:string[]
 
 </script>
 
 
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 <aside class="voltaire flex flex-col bg-indigo-50 gap-2 py-4">
-    {#each items as item}
-        <div class="flex items-center w-10/12 mx-auto">
-            <a  href={`#${deleteBlank(item)}`} class="flex-1">{item}</a><i class="fa-solid fa-arrow-right mtop"></i>
+    {#each items as item, ind}
+        <div class={"flex items-center w-10/12 mx-auto asideItem " + (hoverStatus === ind ? "hovered": "nothovered")} 
+             on:mouseenter={() => { hoverStatus = ind}} 
+             on:mouseleave={() => { hoverStatus = -1}}>
+            <a  href={`#${deleteBlank(item)}`} class="flex-1 mr-1">{item}</a><i class="fa-solid fa-arrow-right mtop mr-2"></i>
         </div>
     {/each}
 
@@ -27,4 +31,17 @@ export let items:string[]
     margin-top: .9%;
 }
 
+.hovered{
+    transition: all .1s;
+    transform: translateY(10%);
+}
+
+.nothovered{
+    display: flex;
+}
+
+.asideItem{
+    border: .2px solid rgb(138, 138, 173);
+    padding: 1%;
+}
 </style>
