b406e84d7e5ea2b3c2c06fda1949c3830ab8ff8d
stupnikk <andre.lacassagne82@gmail.com>
2023-08-09 15:16:31 +0200

    koko

--- src/katex/katex.svelte
+++ src/katex/katex.svelte
@@ -1,21 +1,34 @@
 <script lang="ts">
-    import katexify from "../functions/Katexify";
 
-    export let text:string
-    export let center: boolean
+    import katex from "katex";
+    import { onMount } from "svelte";
+  
+    export let text: string;
+    export let tailwind:string; 
+  
+    let mathElement:HTMLElement; 
+    
+  
+  
+    function renderMath(text:string) {
+      try {
+        // Convert LaTeX math expression to HTML using katex function
+        const htmlContent = katex.renderToString(text, { displayMode: false });
+        mathElement.innerHTML = htmlContent;
+      } catch (error) {
+        // Handle any errors that occur during rendering
+        console.error("Error rendering math:", error);
+        mathElement.innerHTML = ""; // Clear the content in case of errors
+      }
 
-    $: display = center?  "block": "flex"
-
-</script>
-
-
-<div class="katex" style="--display: {display}">
-    {@html katexify(`${text}`, true)}
-</div>
-
-<style>
-
-    .katex{
-        display: var(--display)
     }
-</style>
+    
+    onMount(() => renderMath(text)) 
+
+  </script>
+  
+  <div class={tailwind} bind:this={mathElement}></div>
+  
+  <style>
+    
+  </style>
