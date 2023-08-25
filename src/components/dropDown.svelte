830dad6c34fa4ec42fe8dd5f3c6d51e7e636ba97
Stupnikjs <n.boudier.ph@gmail.com>
2023-08-25 17:09:22 +0200

    ok

--- src/components/dropDown.svelte
+++ src/components/dropDown.svelte
@@ -14,7 +14,7 @@
 <div  class="relative"
 on:mouseenter={ () => { dropDown = "dropdown"}}  
 on:mouseleave={() => { dropDown = "nodropdown"}}>
-  <a href={`/${deleteBlank(title)}`} class=" h-full text-center bg-red-200 w-full px-5 roboto">{title}</a>
+  <a href={`/${deleteBlank(title)}`} class=" h-full text-center bg-red-200 w-full px-5 text-xl">{title}</a>
   <div class={dropDown} >
       {#each items as item, ind}
       <li class={hoverIndex === ind ? "hover": "nohover"} 
@@ -42,8 +42,8 @@ on:mouseleave={() => { dropDown = "nodropdown"}}>
       display: flex;
       flex-direction: column;
       justify-content: space-around;
-      font-size: .7rem;
-      font-family: "Roboto";
+      font-size: .9rem;
+      
 
       
     }
