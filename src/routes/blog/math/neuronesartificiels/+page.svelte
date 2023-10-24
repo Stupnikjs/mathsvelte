051b08a1dd88c13fbe268288788b3bbc86c1ba19
stupnikk <andre.lacassagne82@gmail.com>
2023-10-24 13:26:53 +0200

    ok

--- src/routes/blog/math/neuronesartificiels/+page.svelte
+++ src/routes/blog/math/neuronesartificiels/+page.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-    import "../../../style/style.css"
+    import "../../../../style/style.css"
     import Katex from "../../../../katex/katex.svelte";
     import "@fontsource/voltaire"
     import Item from "../../../../components/item.svelte";
     import Codecomp from "../../../../components/codecomp.svelte";
     import AsideComp from "../../../../components/AsideComp.svelte";
-    import { onMount } from "svelte";
-    import Svg from "../../../../svg/svg.svelte";
-    import type { neuroneSvg } from "../../../../svg/svg";
     import Neurone from "../../../../svg/neurone.svelte";
 
 
